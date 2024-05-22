6c087f1efc41f6481e6d47ee8ba56cba578e3aa4
Endon55 <anthony@anthonycosenza.com>
2024-05-22 13:43:12 -0600

    Cleaned up old Model Code. Added scene handling into Project.

--- src/main/java/com/anthonycosenza/Project.java
+++ src/main/java/com/anthonycosenza/Project.java
@@ -4,11 +4,15 @@ import com.anthonycosenza.engine.space.ProjectSettings;
 import com.anthonycosenza.engine.space.rendering.Scene;
 import com.anthonycosenza.engine.input.Input;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public abstract class Project
 {
     private final ProjectSettings projectSettings;
     private final String projectName;
-    Scene scene;
+    private final List<Scene> scenes;
+    private int currentScene;
 
     
     public Project(String projectName)
@@ -16,6 +20,8 @@ public abstract class Project
         this.projectName = projectName;
         this.projectSettings = new ProjectSettings();
         settings(projectSettings);
+        scenes = new ArrayList<>();
+        currentScene = 0;
     }
     
     public ProjectSettings getSettings()
@@ -28,9 +34,24 @@ public abstract class Project
         return projectName;
     }
     
+    protected void addScene(Scene scene)
+    {
+        this.scenes.add(scene);
+    }
+    
+    protected void setCurrentScene(Scene scene)
+    {
+        currentScene = scenes.indexOf(scene);
+    }
+    
+    protected void setCurrentScene(int sceneIndex)
+    {
+        if(sceneIndex < 0 || sceneIndex >= scenes.size()) throw new ArrayIndexOutOfBoundsException("Scene index is out of bounds: " + sceneIndex);
+        currentScene = sceneIndex;
+    }
     public Scene getScene()
     {
-        return scene;
+        return scenes.get(currentScene);
     }
     abstract public void settings(ProjectSettings settings);
     abstract public void initialize(int width, int height);
--- src/main/java/com/anthonycosenza/engine/space/entity/Model.java
+++ src/main/java/com/anthonycosenza/engine/space/entity/Model.java
@@ -4,13 +4,11 @@ package com.anthonycosenza.engine.space.entity;
 import com.anthonycosenza.engine.space.entity.texture.Material;
 import com.anthonycosenza.engine.space.entity.texture.Texture;
 
-import java.util.ArrayList;
 import java.util.List;
 
 public class Model
 {
     private List<Material> materials;
-    private List<Mesh> meshes;
     private Texture texture;
     
     public Model(List<Material> materials)
@@ -19,32 +17,9 @@ public class Model
         this.texture = null;
     }
     
-    
-    public Model(List<Mesh> meshes, Texture texture)
-    {
-        this.meshes = new ArrayList<>(meshes);
-        this.texture = texture;
-    }
-    public Model(Mesh mesh)
-    {
-        this.meshes = new ArrayList<>();
-        this.texture = null;
-    }
-    
-    public Model(Mesh mesh, Texture texture)
-    {
-        this.meshes = new ArrayList<>();
-        this.texture = texture;
-    }
-    
     public List<Material> getMaterials()
     {
         return materials;
-    }
-    
-    public List<Mesh> getMeshes()
-    {
-        return meshes;
     }
     
     public Texture getTexture()
